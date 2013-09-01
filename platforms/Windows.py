7b71a91bc714741f74603ac22b70f5f05dfc09d1
Andrei Tuicu <andrei.tuicu@gmail.com>
2013-09-01 17:30:43 +0300

    Working on portability. new file:   platforms/Windows.py

---
+++ platforms/Windows.py
@@ -0,0 +1,25 @@
+"""
+    General description:
+     This module contains the Windows class, which implements Windows
+    platform specific functions.
+    (C) 2013, Andrei Tuicu <andrei.tuicu@gmail.com>
+                 last review 01.09.2013
+"""
+import os
+
+class Windows:
+    def toolIsInstalled(self, tool):
+        for path in os.environ["PATH"].split(os.pathsep):
+            filesInPath = os.listdir(path)
+            for fileName in filesInPath:
+                """not sure yet
+                if tool in fileName and ".exe" in fileName
+                    or tool in fileName and ".EXE in fileName:
+                """
+                if tool + ".exe" == fileName \
+                    or tool + ".EXE" == fileName:
+                    print path
+                    print fileName
+                    return True
+
+        return False
