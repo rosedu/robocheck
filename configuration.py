9c6b51bac377e9938f08b9425c79f3713c16a352
Andrei Tuicu <andrei.tuicu@gmail.com>
2013-10-30 11:33:48 +0200

    Finished configuring the penalties. modified:   configuration.py

--- configuration.py
+++ configuration.py
@@ -14,6 +14,91 @@ import os
 language = []
 errorsToLookFor = []
 
+def confirmPenaltyConfigurations( penaltyDictionary ):
+    os.system('clear')
+    print 'Error Type',
+    for i in range(15):
+        print ' ',
+    print 'Mode[S/M] ',
+    print 'Occurences ',
+    print 'Penalty'
+    print ''
+
+    for error in penaltyDictionary:
+        print error,
+        for i in range( (45-len(error))/2 ):
+            print ' ',
+        if penaltyDictionary[error][0] is None:
+            print 'S',
+            for i in range(4):
+                print ' ',
+            print ' ',
+        else:
+            print 'M',
+            for i in range(4):
+                print ' ',
+            print penaltyDictionary[error][0],
+
+        for i in range(4):
+            print ' ',
+        print penaltyDictionary[error][1]
+
+    print '\nIs this configuration correct? [Y/n]',
+    option = sys.stdin.readline().splitlines()[0]
+    if option != 'N' and option != 'n':
+        return True
+    else:
+        return False
+
+def penaltyConfig( errorsToLookFor ):
+    penaltyDictionary = dict()
+    os.system('clear')
+    for error in errorsToLookFor:
+        print error
+        while True:
+            print "\tMode[S/M]:  ",
+            mode = sys.stdin.readline().splitlines()[0].upper()
+            if mode != "S" and mode != "M":
+                print "Invalid option!"
+            else:
+                break
+
+        if mode == "M":
+            while True:
+                print "\tOccurences: ",
+                occurence = sys.stdin.readline().splitlines()[0]
+                if occurence.isdigit():
+                    occurence = int(occurence)
+                    if occurence <= 0:
+                        print "Invalid option! Need a number > 0."
+                    else:
+                        break
+                else:
+                    print "Invalid option! Need a number."
+        else:
+            occurence = None
+
+
+        while True:
+            print "\tPenalty:    ",
+            penalty = sys.stdin.readline().splitlines()[0]
+            try:
+                penalty = float(penalty)
+                if penalty < 0:
+                    print "Invalid option! Need a float >= 0."
+                else :
+                    break
+            except ValueError:
+                print "Invalid option! Need a float number."
+        penaltyDictionary[error] = [occurence, penalty]
+        print ''
+
+    if confirmPenaltyConfigurations( penaltyDictionary) is True:
+        return penaltyDictionary
+    else:
+        return penaltyConfig( errorsToLookFor )
+
+
 def createConfigFile():
     os.system('clear')
     returnPath = os.getcwd()
@@ -74,14 +159,23 @@ def createConfigFile():
             print "[ ]",
         print error
 
+    
     print "\nIs this configuration correct? [Y/n]:"
     option = sys.stdin.readline().splitlines()[0]
     if option is "N" or option is "n":
-        createconfig.json()
+        createConfigFile()
     else:
+        print "Do you want to add penalties for errors? [Y/n]"
+        option = sys.stdin.readline().splitlines()[0]
+        if option is "N" or option is "n":
+            penalty = False
+        else:
+            penalty = True
+            toLookFor = penaltyConfig( toLookFor )
+
         config = open('config.json', 'w')
         jsonList = []
-        jsonList.append( { 'Language': language, 'Errors': toLookFor} )
+        jsonList.append( { 'Language': language, 'Errors': toLookFor, 'Penalty' : penalty } )
         config.write(json.dumps(jsonList, indent=2))
         config.close()
         return True
